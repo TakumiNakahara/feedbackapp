fb54560eebff8b087eebe1608ddcb2bed68278f2
TakumiNakahara <XXXX@hogehoge.com>
2022-05-31 22:28:14 +0900

    propsについて

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from "react"
+import Header from "./containers/header"
 
 function App(){
-    const title = 'Blog Post'
-    const body = 'this is my blog post'
-    const comments = [
-        {id: 1, text: 'one'},
-        {id: 2, text: 'two'},
-        {id: 3, text: 'three'}
-    ]
-    const showCommnents = true
-    const commentBlock = (
-        <div className="comments">
-            <h3>Comments({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index)=>(
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
+    return (
+        <>
+            <Header text={true} />
+            <div className="container">
+                <h1>MyApp</h1>
+            </div>
+        </>
     )
-    return(
-        <div className="container">
-            <h1>{title.toUpperCase}</h1>
-            <p>{body}</p>
-
-            {showCommnents && commentBlock}
-        </div>
-        )
 }
 
 export default App 
