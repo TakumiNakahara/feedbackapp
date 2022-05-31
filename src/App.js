bcab3dfb59e10cffa4eefa6871f1dbd42ee546b9
TakumiNakahara <XXXX@hogehoge.com>
2022-05-31 21:49:08 +0900

    mapメソッドを使って、配列をDOMで出力する方法

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React from "react"
+
 function App(){
-    return <h1>Hello from App component</h1>
+    const title = 'Blog Post'
+    const body = 'this is my blog post'
+    const comments = [
+        {id: 1, text: 'one'},
+        {id: 2, text: 'two'},
+        {id: 3, text: 'three'}
+    ]
+    return(
+        <div className="container">
+            <h1>{title.toUpperCase}</h1>
+            <p>{body}</p>
+            <div className="comments">
+                <h3>Comments({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index)=>(
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+        )
 }
 
-export default App
+export default App 
